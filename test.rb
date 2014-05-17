e6825ee648e08b9a06983a50ee13718a80c1ec96
Andrew Gilbert <andrewg800@gmail.com>
2014-05-16 23:48:18 -0700

    Cleanup

--- test.rb
+++ test.rb
@@ -13,7 +13,7 @@ class Test
 	def argmeth arg1
 		puts "Argmeth is demanding.\nIt wants one argument. You said #{arg1}"
 	end
-	def paramtest(arg,arg2="default",arg3="test",*arg4)
+	def paramtest(arg, arg2="default", arg3="test", *arg4)
 		puts "Arguments:|#{arg}|#{arg2}|#{arg3}|#{arg4}|"
 	end
 	def newtest
